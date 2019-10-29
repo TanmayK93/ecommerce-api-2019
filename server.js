21f338402e695c627f6ee546af740aa4620e8ada
Tanmay Kolge <tanmaykolge93@gmail.com>
2019-10-29 17:08:23 -0400

    make it better2

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = 4000;
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const config = require('./connect.js');
+const config = require("./connect.js");
 
-const productRoute = require('../api/routes/product-routes.js');
-const userRoute = require('../api/routes/user-routes.js');
-const cartRoute = require('../api/routes/cart-routes.js');
-const orderRoute = require('../api/routes/order-routes.js');
-const commentRoute = require('../api/routes/comment-routes.js');
+const productRoute = require("./routes/product-routes");
+const userRoute = require("./routes/user-routes.js");
+const cartRoute = require("./routes/cart-routes.js");
+const orderRoute = require("../routes/order-routes.js");
+const commentRoute = require("./routes/comment-routes.js");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => {
-    console.log('Database is connected') 
+    console.log("Database is connected");
   },
-  err => { 
-    console.log('Can not connect to the database'+ err)
+  err => {
+    console.log("Can not connect to the database" + err);
   }
 );
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use("/posts", productRoute);
+app.use("/users", userRoute);
+app.use("/carts", cartRoute);
+app.use("/orders", orderRoute);
+app.use("/comments", commentRoute);
 
-app.use('/posts', productRoute);
-app.use('/users',userRoute);
-app.use('/carts',cartRoute);
-app.use('/orders',orderRoute);
-app.use('/comments',commentRoute);
-
-app.listen(PORT, function(){
-  console.log('Server is running on Port:',PORT);
+app.listen(PORT, function() {
+  console.log("Server is running on Port:", PORT);
 });
