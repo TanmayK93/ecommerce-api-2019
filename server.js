c549bf77fe764f6435545027f8924effec06e72b
Tanmay Kolge <tanmaykolge93@gmail.com>
2020-04-06 20:37:12 -0400

    fix route

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/posts", productRoute);
+app.use("/products", productRoute);
 app.use("/coffee", coffeeRoute);
 app.use("/users", userRoute);
 app.use("/carts", cartRoute);
